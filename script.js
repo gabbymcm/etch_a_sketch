ff276b5d488e527e31ad838f4f19a446d561b9f4
Gab <gabby.mcm@gmail.com>
2021-12-02 19:36:38 -0500

    Update etch-a-sketch to keep a constant sized container for the grid but to have the squares change size depending on the user selection

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const container = document.querySelector('#container');
 
+function makeGrid(size) {
+    for (i=1; i <= size*size; i++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+        square.style.width = 500 / size + "px";
+        square.style.height = 500 / size + "px";
 
-for (i=1; i <= 256; i++){
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+        square.addEventListener('mouseenter', function(){
+            square.classList.add("active");
+        });
+    }
 
-    square.addEventListener('mouseenter', function(e){
-        e.target.style.background = 'black';
-    });
-
+    container.style.width = "500px";
+    container.style.height = "500px";
 }
+
+makeGrid(16, 20);
 
 const btn = document.querySelector('#newGame');
 btn.addEventListener('click', () => {
     
-
     function removeAllChildNodes(parent){
         while(parent.firstChild) {
             parent.removeChild(parent.firstChild);
@@ -27,18 +33,8 @@ btn.addEventListener('click', () => {
     var quantity = prompt("Please enter the number of squares for your grid:");
 
     if (quantity <= 100){
-        for (i=1; i <= (quantity*quantity); i++){
-            const square = document.createElement('div');
-            square.classList.add('square');
-            container.appendChild(square);
-        
-            square.addEventListener('mouseenter', function(e){
-                e.target.style.background = 'black';
-            });
-        };
+        makeGrid(quantity);
     } else {
         alert("Please try again with a number less than 100.")
     }
 });
-
-
